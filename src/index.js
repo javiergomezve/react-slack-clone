10f07df7dc035c848f14c47d5a19308d850bb7cd
Javier Gomez <javiergomezve@gmail.com>
2018-11-27 12:05:01 -0400

    Auth components and routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css';
+import * as ROUTES from './constants/routes';
 import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path={ROUTES.HOME} component={App} />
+      <Route path={ROUTES.LOGIN} component={Login} />
+      <Route path={ROUTES.REGISTER} component={Register} />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
