605780d1db2137d4efb058cb2f3c6e985bba94e9
Javier Gomez <javiergomezve@gmail.com>
2018-12-02 02:03:31 -0400

    ?Set public channels

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 import firebase from '../../firebase';
-import { setCurrentChannel } from '../../actions';
+import { setCurrentChannel, setPrivateChannel } from '../../actions';
 
 class Channels extends Component {
   state = {
@@ -92,6 +92,7 @@ class Channels extends Component {
   changeChannel = channel => {
     this.setActiveChannel(channel);
     this.props.setCurrentChannel(channel);
+    this.props.setPrivateChannel(false);
   };
 
   setActiveChannel = channel => {
@@ -162,4 +163,4 @@ class Channels extends Component {
   }
 }
 
-export default connect(null, { setCurrentChannel })(Channels);
+export default connect(null, { setCurrentChannel, setPrivateChannel })(Channels);
