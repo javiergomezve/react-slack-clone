8bc61f6c4712245048c6a599da7e99a053654975
Javier Gomez <javiergomezve@gmail.com>
2018-12-02 07:59:39 -0400

    Add notifications to public channels

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import { Menu, Icon, Modal, Form, Input, Button, Label } from 'semantic-ui-react';
 import firebase from '../../firebase';
 import { setCurrentChannel, setPrivateChannel } from '../../actions';
 
@@ -8,10 +8,13 @@ class Channels extends Component {
   state = {
     activeChannel: '',
     user: this.props.currentUser,
+    channel: null,
     channels: [],
     channelName: '',
     channelDetails: '',
     channelsRef: firebase.database().ref('channels'),
+    messagesRef: firebase.database().ref('messages'),
+    notifications: [],
     modal: false,
     firstLoad: true
   };
@@ -31,7 +34,43 @@ class Channels extends Component {
       loadedChannels.push(snap.val());
       
       this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
+      this.addNotificationListener(snap.key);
     });
+  };
+
+  addNotificationListener = channelId => {
+    this.state.messagesRef.child(channelId).on('value', snap => {
+      if (this.state.channel) {
+        this.handleNotifications(channelId, this.state.channel.id, this.state.notifications, snap);
+      }
+    });
+  };
+
+  handleNotifications = (channelId, currentChannelId, notifications, snap) => {
+    let lastTotal = 0;
+
+    let index = notifications.findIndex(notification => notification.id === channelId);
+
+    if (index !== -1) {
+      if (channelId !== currentChannelId) {
+        lastTotal = notifications[index].total;
+
+        if (snap.numChildren() - lastTotal > 0) {
+          notifications[index].count = snap.numChildren() - lastTotal;
+        }
+      }
+
+      notifications[index].lastKnowTotal = snap.numChildren();
+    } else {
+      notifications.push({
+        id: channelId,
+        total: snap.numChildren(),
+        lastKnowTotal: snap.numChildren(),
+        count: 0
+      });
+    }
+
+    this.setState({ notifications });
   };
 
   removeListeners = () => {
@@ -44,6 +83,7 @@ class Channels extends Component {
     if (this.state.firstLoad && this.state.channels.length > 0) {
       this.props.setCurrentChannel(firstChannel);
       this.setActiveChannel(firstChannel);
+      this.setState({ channel: firstChannel });
     }
 
     this.setState({ firstLoad: false });
@@ -75,6 +115,18 @@ class Channels extends Component {
       });
   };
 
+  getNotificationCount = channel => {
+    let count = 0;
+
+    this.state.notifications.forEach(notification => {
+      if (notification.id === channel.id) {
+        count = notification.count;
+      }
+    });
+
+    if (count > 0) return count;
+  };
+
   displayChannels = channels => (
     channels.length > 0 && channels.map(channel => (
       <Menu.Item
@@ -84,6 +136,9 @@ class Channels extends Component {
         style={{ opacity: 0.7 }}
         active={channel.id === this.state.activeChannel}
       >
+        {this.getNotificationCount(channel) && (
+          <Label color="red">{this.getNotificationCount(channel)}</Label>
+        )}
         # {channel.name}
       </Menu.Item>
     ))
@@ -91,8 +146,21 @@ class Channels extends Component {
 
   changeChannel = channel => {
     this.setActiveChannel(channel);
+    this.clearNotifications();
     this.props.setCurrentChannel(channel);
     this.props.setPrivateChannel(false);
+    this.setState({ channel });
+  };
+
+  clearNotifications = () => {
+    let index = this.state.notifications.findIndex(notification => notification.id === this.state.channel.id);
+
+    if (index !== -1) {
+      let updateNotifications = [...this.state.notifications];
+      updateNotifications[index].total = this.state.notifications[index].lastKnowTotal;
+      updateNotifications[index].count = 0;
+      this.setState({ notifications: updateNotifications });
+    }
   };
 
   setActiveChannel = channel => {
